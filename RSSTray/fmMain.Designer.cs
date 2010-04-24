5419449e4141c02a59b8e343f068d9c430714839
David Jones <david@metaburbia.com>
2010-04-24 12:58:51 +0100

    2nd commit before push

--- RSSTray/fmMain.Designer.cs
+++ RSSTray/fmMain.Designer.cs
@@ -29,23 +29,72 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fmMain));
             this.NotifyRSSTray = new System.Windows.Forms.NotifyIcon(this.components);
+            this.menuContextMain = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.showToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuContextMain.SuspendLayout();
             this.SuspendLayout();
             // 
             // NotifyRSSTray
             // 
+            this.NotifyRSSTray.ContextMenuStrip = this.menuContextMain;
+            this.NotifyRSSTray.Icon = ((System.Drawing.Icon)(resources.GetObject("NotifyRSSTray.Icon")));
             this.NotifyRSSTray.Text = "NotifyRSSTray";
             this.NotifyRSSTray.Visible = true;
+            // 
+            // menuContextMain
+            // 
+            this.menuContextMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showToolStripMenuItem,
+            this.closeToolStripMenuItem1,
+            this.toolStripMenuItem1,
+            this.closeToolStripMenuItem});
+            this.menuContextMain.Name = "menuContextMain";
+            this.menuContextMain.Size = new System.Drawing.Size(104, 76);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.closeToolStripMenuItem.Text = "Exit";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem1
+            // 
+            this.closeToolStripMenuItem1.Name = "closeToolStripMenuItem1";
+            this.closeToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.closeToolStripMenuItem1.Text = "Close";
+            this.closeToolStripMenuItem1.Click += new System.EventHandler(this.closeToolStripMenuItem1_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // showToolStripMenuItem
+            // 
+            this.showToolStripMenuItem.Name = "showToolStripMenuItem";
+            this.showToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.showToolStripMenuItem.Text = "Show";
+            this.showToolStripMenuItem.Click += new System.EventHandler(this.showToolStripMenuItem_Click);
             // 
             // fmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(359, 488);
+            this.ClientSize = new System.Drawing.Size(443, 488);
+            this.ContextMenuStrip = this.menuContextMain;
+            this.ControlBox = false;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "fmMain";
             this.ShowInTaskbar = false;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "RSS Items";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Load += new System.EventHandler(this.fmMain_Load);
+            this.menuContextMain.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -53,6 +102,11 @@
         #endregion
 
         private System.Windows.Forms.NotifyIcon NotifyRSSTray;
+        private System.Windows.Forms.ContextMenuStrip menuContextMain;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem showToolStripMenuItem;
     }
 }
 
--- RSSTray/fmMain.cs
+++ RSSTray/fmMain.cs
@@ -14,6 +14,44 @@ namespace RSSTray
         public fmMain()
         {
             InitializeComponent();
+            this.Visible = false;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void closeToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void showToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            RestoreWindow();
+        }
+
+        private void fmMain_Load(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+            Visible = false;
+     
+        }
+
+
+        private void RestoreWindow()
+        {
+            Rectangle rect = new Rectangle();
+            WindowState = FormWindowState.Normal;
+
+
+            MessageBox.Show(this.Width.ToString());
+            
+            this.Left = Screen.PrimaryScreen.Bounds.Width  - this.Width;
+   
+
+       
         }
     }
 }
